ecc0f0427bf43417f91d549a8420c55e072c451a
inknos <nicola.sella@outlook.it>
2019-08-21 14:37:40 +0200

    first commit before github

--- gulpfile.js
+++ gulpfile.js
@@ -8,50 +8,73 @@ const merge = require("merge-stream");
 
 // BrowserSync
 function browserSync(done) {
-  browsersync.init({
-    server: {
-      baseDir: "./"
-    },
-    port: 3000
-  });
-  done();
+    browsersync.init({
+        server: {
+            baseDir: "./"
+        },
+        port: 3000
+    });
+    done();
 }
 
 // BrowserSync reload
 function browserSyncReload(done) {
-  browsersync.reload();
-  done();
+    browsersync.reload();
+    done();
 }
 
 // Clean vendor
 function clean() {
-  return del(["./vendor/"]);
+    return del(["./vendor/"]);
 }
 
 // Bring third party dependencies from node_modules into vendor directory
 function modules() {
-  // Bootstrap
-  var bootstrap = gulp.src('./node_modules/bootstrap/dist/**/*')
-    .pipe(gulp.dest('./vendor/bootstrap'));
-  // Popper.js
-  var pooper = gulp.src('./node_modules/popper.js/dist/*')
-    .pipe(gulp.dest('./vendor/popper.js'));
-  // jQuery
-  var jquery = gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
+    // Bootstrap
+    var bootstrap = gulp.src('./node_modules/bootstrap/dist/**/*')
+        .pipe(gulp.dest('./vendor/bootstrap'));
+    // Popper.js
+    var popperjs = gulp.src('./node_modules/popper.js/dist/*')
+        .pipe(gulp.dest('./vendor/popper.js'));
+    // jQuery
+    var jquery = gulp.src([
+        './node_modules/jquery/dist/*',
+        '!./node_modules/jquery/dist/core.js'
     ])
-    .pipe(gulp.dest('./vendor/jquery'));
-  // jQuery Easing
-  var jqueryEasing = gulp.src('./node_modules/jquery.easing/*.js')
-    .pipe(gulp.dest('./vendor/jquery-easing'));
-  return merge(bootstrap, jquery, jqueryEasing);
+        .pipe(gulp.dest('./vendor/jquery'));
+    // jQuery Easing
+    var jqueryEasing = gulp.src('./node_modules/jquery.easing/*.js')
+        .pipe(gulp.dest('./vendor/jquery-easing'));
+    // jQuery i18next
+    var jqueryi18next = gulp.src('./node_modules/jquery-i18next/*.js')
+        .pipe(gulp.dest('./vendor/jquery-i18next'));
+    var i18next = gulp.src('./node_modules/i18next/*.js')
+        .pipe(gulp.dest('./vendor/i18next'));
+    // i18next XHR bckend
+    var i18nextXHR = gulp.src('./node_modules/i18next-xhr-backend/*.js')
+        .pipe(gulp.dest('./vendor/i18next-xhr-backend'));
+    // jQuery parallax
+    var jqueryparallax = gulp.src('./node_modules/jquery-parallax/scripts/*.js')
+        .pipe(gulp.dest('./vendor/jquery-parallax'));
+    // jQuery countTo
+    var countto = gulp.src('./node_modules/jquery-countto/*.js')
+        .pipe(gulp.dest('./vendor/jquery-countto'));
+    return merge(
+        bootstrap,
+        popperjs,
+        jquery,
+        jqueryEasing,
+        jqueryi18next,
+        i18nextXHR,
+        jqueryparallax,
+        countto
+    );
 }
 
 // Watch files
 function watchFiles() {
-  gulp.watch("./**/*.css", browserSyncReload);
-  gulp.watch("./**/*.html", browserSyncReload);
+    gulp.watch("./**/*.css", browserSyncReload);
+    gulp.watch("./**/*.html", browserSyncReload);
 }
 
 // Define complex tasks
---
+++ js/translate.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    i18next
+        .use(i18nextXHRBackend)
+        .init({
+            fallbackLng: 'en',
+            debug: true,
+            ns: ['translation', 'debug'],
+            defaultNS: 'debug',
+            backend: {
+                loadPath: '/locales/{{lng}}/{{ns}}.json',
+                crossDomain: true
+            }
+        }, function(err, t) {
+            updateContent();
+        });
+});
+
+
+function updateContent(){
+    jqueryI18next.init(i18next, $, {
+        tName: 't', // --> appends $.t = i18next.t
+        i18nName: 'i18n', // --> appends $.i18n = i18next
+        handleName: 'localize', // --> appends $(selector).localize(opts);
+        selectorAttr: 'data-i18n', // selector for translating elements
+        targetAttr: 'i18n-target', // data-() attribute to grab target element to translate (if different than itself)
+        optionsAttr: 'i18n-options', // data-() attribute that contains options, will load/set if useOptionsAttr = true
+        useOptionsAttr: false, // see optionsAttr
+        parseDefaultValueFromContent: true // parses default values from content ele.val or ele.text
+    });
+    // start localizing, details:
+    $('.nav').localize();
+    // https://github.com/i18next/jquery-i18next#usage-of-selector-function
+}
+
+
+function changeLng(lng) {
+    i18next.changeLanguage(lng);
+}
